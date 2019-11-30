39eb2b7de544ff43a6dc3b7b377a23aefbfc8a44
Matthew Croak <matthewcroak@Matthews-MBP.fios-router.home>
2019-11-30 11:44:51 -0500

    made App functional

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import './App.css';
 import SizeContainer from './components/SizeContainer.js'
 import Buttons from './components/Buttons.js'
 
-class App extends Component{
+const App = () => {
 
-  state = {
-    isSizeStateChanged: false
-  };
-
-  render(){
     return (
       <div className="App">
         <h1 style={{height: 40, marginBottom: 10}}>React Editor</h1>
-        <SizeContainer isSizeStateChanged={this.state.isSizeStateChanged} onChangeSizeTrue={this.onChangeSizeTrue}/>
+        <SizeContainer/>
       </div>
     );
-  }
-}
+    }
 
 export default App;
